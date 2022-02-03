dbc4bceef8d6dacf7bdbfa2ff41b69d2987af2b4
Shivam Kumar <shivam.shah2701@gmail.com>
2022-02-03 14:46:11 +0530

    Click Listener Added

--- app/src/main/java/com/example/ud839_miwok_lesson_one/MainActivity.java
+++ app/src/main/java/com/example/ud839_miwok_lesson_one/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView numberActivity=(TextView) findViewById(R.id.numbers);
+        numberActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void openNumbersView(View view){
-        Intent intent = new Intent(this,NumbersActivity.class);
-        startActivity(intent);
-
-    }
+//    public void openNumbersView(View view){
+//        Intent intent = new Intent(this,NumbersActivity.class);
+//        startActivity(intent);
+//
+//    }
 }
